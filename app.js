ae14e6f66d0257bb74ab50a24ea990cb4c2d1fb9
vidit Khemka <viditkhemka63@gmail.com>
2019-04-05 07:06:21 +0530

    Adding  authentication using passport

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var configDB = require('./config/database.js');
 
-var app = express();
+// configuration ===============================================================
+mongoose.connect(configDB.url, (err) => {
+    if(err) return err;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    console.log('database connected');
+}); // connect to our database
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+ require('./config/passport')(passport); // pass passport for configuration
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-module.exports = app;
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
